8ef6300ffbb5797364faf1a17b8df50edea756f0
sohamjdv-7239 <soham0266@gmail.com>
2025-07-23 14:34:47 +0530

    242. Valid Anagram

---
+++ anagram.cpp
@@ -0,0 +1,15 @@
+#include<algorithm>
+#include<iostream>
+using namespace std;
+
+
+class Solution {
+public:
+    
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length())return false;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
+    }
+};
