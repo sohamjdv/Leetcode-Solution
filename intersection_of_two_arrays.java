53f8e79e40a2b8733d956f0171b4a4f43b320129
sohamjdv-7239 <sohamjdv0203@gmail.com>
2025-08-02 22:09:10 +0530

    349. Intersection of Two Arrays

---
+++ intersection_of_two_arrays.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer>set=new HashSet<>();
+        Set<Integer>reSet=new HashSet<>();
+        for(int i:nums1){
+            set.add(i);
+        }
+        for(int i:nums2){
+            if(set.contains(i)){
+                set.remove(i);
+                reSet.add(i);
+            }
+        }
+        int []res=new int [reSet.size()];
+        int j=0;
+        for(int i:reSet){
+            res[j++]=i;
+        }
+        return res;
+    }
+}
