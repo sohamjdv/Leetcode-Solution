6bcb0027c770c0ae8511439bb6c739cb847cf41b
sohamjdv-7239 <sohamjdv0203@gmail.com>
2025-08-12 18:47:37 +0530

    14. Longest Common Prefix

---
+++ longest_commom_string.java
@@ -0,0 +1,22 @@
+class Solution14 {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs==null||strs.length==0)return "";
+        String pref=strs[0];
+        int preflen=pref.length();
+        for(int i=1;i<strs.length;i++){
+            String s=strs[i];
+            while(preflen>s.length()|| !pref.equals(s.substring(0,preflen))){
+                preflen--;
+                if(preflen==0){
+                    return "";
+                }
+                pref=pref.substring(0,preflen);
+            }
+        }
+        return pref;
+    }
+}
+
+public class longest_commom_string {
+    
+}
